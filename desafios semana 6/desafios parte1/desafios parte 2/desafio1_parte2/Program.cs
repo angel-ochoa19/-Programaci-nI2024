3b0728adb16a5cb6abbbbedbb13c65c4f731f037
angel-ochoa19 <135042636+angel-ochoa19@users.noreply.github.com>
2024-03-15 23:15:05 -0600

    Add files via upload

---
+++ desafios semana 6/desafios parte1/desafios parte 2/desafio1_parte2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+public class Rectangulo
+{
+    private readonly double Alto;
+    private readonly double Largo;
+
+    public Rectangulo(double alto, double largo)
+    {
+        Alto = alto;
+        Largo = largo;
+    }
+
+    public double SuperficieFrontal
+    {
+        get
+        {
+            return Alto * Largo;
+        }
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Rectangulo rectangulo = new Rectangulo(5, 10);
+        Console.WriteLine("La superficie frontal del rectángulo es: " + rectangulo.SuperficieFrontal);
+    }
+}
