ea356b5d51babfa9c1ef5c871ec76ed8438b8987
angel-ochoa19 <135042636+angel-ochoa19@users.noreply.github.com>
2024-04-19 23:19:10 -0600

    Add files via upload

---
+++ Desafio semana 10/Windows_form.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Windows.Forms;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace MiAplicacion
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            // Add code that will execute when the form is loaded
+            // For example, you can initialize some controls or load data
+        }
+
+        private void btnMostrarMensaje_Click(object sender, EventArgs e)
+        {
+            // Display an informational message when the user clicks the button
+            MessageBox.Show("¡Hola! Has presionado el botón para mostrar un mensaje.", "Mensaje", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void btnCerrar_Click(object sender, EventArgs e)
+        {
+            // Confirm with the user before closing the application
+            DialogResult result = MessageBox.Show("¿Estás seguro de que quieres cerrar la aplicación?", "Confirmación", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+            if (result == DialogResult.Yes)
+            {
+                Application.Exit();
+            }
+        }
+    }
+
+    static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new MainForm());
+        }
+    }
+}
