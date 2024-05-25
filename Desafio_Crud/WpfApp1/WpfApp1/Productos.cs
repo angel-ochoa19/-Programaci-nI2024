b0e0f3b8b1ee1312586b832ce125200b49a3ac79
angel-ochoa19 <135042636+angel-ochoa19@users.noreply.github.com>
2024-05-24 21:32:08 -0600

    Add files via upload

---
+++ Desafio_Crud/WpfApp1/WpfApp1/Productos.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1
+{
+    internal class Productos
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public int Precio { get; set; }
+    }
+}
---
+++ Desafio_Crud/WpfApp1/WpfApp1/Ventas.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1
+{
+    public class ventas
+    {
+        public int Id { get; set; }
+        public string Cliente { get; set; }
+        public int ID_producto { get; set; }
+        public string Cantidad { get; set; }
+        public int Precio { get; set; }
+    }
+}
